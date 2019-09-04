b8e2a327dee12967a4c95923ee2b2f98626c3379
nvpopov <brodiaga38@gmail.com>
2019-09-04 23:57:42 +0800

    fix: doc v2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,8 @@
 
 
 # -- Project information -----------------------------------------------------
-
+master_doc = 'index'
+source_suffix = '.rst'
 project = 'qppcad'
 copyright = '2019, N.Popov'
 author = 'N.Popov'
@@ -28,6 +29,7 @@ author = 'N.Popov'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
